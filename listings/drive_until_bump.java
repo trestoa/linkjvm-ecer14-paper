dd704b8bc1a7fa8d775be1fe87d4a0ea1d9c5813
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2014-03-22 23:43:31 +0100

    Added drive_until_bump.java

---
+++ listings/drive_until_bump.java
@@ -0,0 +1,12 @@
+public static void main(String[] args){
+	Motor leftMotor = new Motor(0);
+	Motor rightMotor = new Motor(1);
+	DigitalSensor bumpSensor = new DigitalSensor(0);
+	leftMotor.turn(100);
+	rightMotor.turn(100);
+	while(!bumpSensor.getValue()){
+		Botball.msleep(100);
+	}
+	leftMotor.freeze();
+	rightMotor.freeze();		
+}
