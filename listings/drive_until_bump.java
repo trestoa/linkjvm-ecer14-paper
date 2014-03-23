22a1de8d3ff828a8d80c21b1241a267ce4104165
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2014-03-23 15:28:45 +0100

    Some visual improvements

--- listings/drive_until_bump.java
+++ listings/drive_until_bump.java
@@ -1,7 +1,8 @@
 public static void main(String[] args){
 	Motor leftMotor = new Motor(0);
 	Motor rightMotor = new Motor(1);
-	DigitalSensor bumpSensor = new DigitalSensor(0);
+	DigitalSensor bumpSensor = 
+	new DigitalSensor(0);
 	leftMotor.turn(100);
 	rightMotor.turn(100);
 	while(!bumpSensor.getValue()){
--- listings/vision.java
+++ listings/vision.java
@@ -1,18 +1,20 @@
 public static void main(String[] args) {
 	Create create = new Create();
 	create.connect();
-	camera = new ImageProcessor(new CameraConfig(Resolution.MED_RES), 0);
+	camera = new ImageProcessor(
+	new CameraConfig(Resolution.MED_RES), 0);
 	camera.openCamera();
 	SideButton sb = new SideButton();
 	while(!sb.getValue()) {
 		camera.update();
 		if(camera.getObjectCount() > 0) {
-			Rectangle rec = camera.getBoundingBox(0);
+			Rectangle rec = 
+			camera.getBoundingBox(0);
 			if(rec.getWidth() > 80){
 				if(rec.getCenter().getX() < 290){
 					create.driveDirect(100, 0);
-				}
-				else if(rec.getCenter().getX() > 350){
+				}else
+				if(rec.getCenter().getX() > 350){
 					create.driveDirect(100, 0);
 				}
 				Botball.msleep(500);
